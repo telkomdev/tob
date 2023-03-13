94e31b64dc5d8db5ec0f9b8f7245b176617db593
wuriyanto <wuriyanto48@yahoo.co.id>
2023-03-13 19:19:03 +0700

    util: add interface to float

---
+++ util/util.go
@@ -0,0 +1,57 @@
+package util
+
+import (
+	"strconv"
+)
+
+func InterfaceToFloat64(val interface{}) float64 {
+
+	var i float64
+	switch t := val.(type) {
+	case int:
+		i = float64(t)
+		break
+	case int8:
+		i = float64(t)
+		break
+	case int16:
+		i = float64(t)
+		break
+	case int32:
+		i = float64(t)
+		break
+	case int64:
+		i = float64(t)
+		break
+	case float32:
+		i = float64(t)
+		break
+	case float64:
+		i = float64(t)
+		break
+	case uint8:
+		i = float64(t)
+		break
+	case uint16:
+		i = float64(t)
+		break
+	case uint32:
+		i = float64(t)
+		break
+	case uint64:
+		i = float64(t)
+		break
+	case string:
+		num, err := strconv.Atoi(t)
+		if err != nil {
+			return 0.0
+		}
+
+		i = float64(num)
+		break
+	default:
+		i = 0.0
+	}
+
+	return i
+}
